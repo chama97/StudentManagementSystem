b818ebf31df8b01893ae4e9a02097f7f09c1d6e7
chama97 <chamamanchanayake5@gmail.com>
2022-07-12 10:25:14 +0530

    table model student

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,4 +1,82 @@
 package model;
 
 public class Student {
+    private String student_Id;
+    private String student_name;
+    private String email;
+    private String contact;
+    private String address;
+    private String nic;
+
+    public Student() {
+    }
+
+    public Student(String student_Id, String student_name, String email, String contact, String address, String nic) {
+        this.setStudent_Id(student_Id);
+        this.setStudent_name(student_name);
+        this.setEmail(email);
+        this.setContact(contact);
+        this.setAddress(address);
+        this.setNic(nic);
+    }
+
+    public String getStudent_Id() {
+        return student_Id;
+    }
+
+    public void setStudent_Id(String student_Id) {
+        this.student_Id = student_Id;
+    }
+
+    public String getStudent_name() {
+        return student_name;
+    }
+
+    public void setStudent_name(String student_name) {
+        this.student_name = student_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "student_Id='" + student_Id + '\'' +
+                ", student_name='" + student_name + '\'' +
+                ", email='" + email + '\'' +
+                ", contact='" + contact + '\'' +
+                ", address='" + address + '\'' +
+                ", nic='" + nic + '\'' +
+                '}';
+    }
 }
