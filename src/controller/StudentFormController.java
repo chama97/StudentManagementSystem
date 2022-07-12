9f7d9178f0caab79a7716a08f4762cad6740f6a5
chama97 <chamamanchanayake5@gmail.com>
2022-07-12 11:03:33 +0530

    UI added

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,4 +1,37 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class StudentFormController {
+    public AnchorPane studentFID;
+    public JFXTextField txtId;
+    public JFXTextField txtMail;
+    public JFXTextField txtName;
+    public JFXTextField txtContact;
+    public JFXTextField txtAddress;
+    public JFXTextField txtNic;
+    public JFXTextField txtSearch;
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colMail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
 }
